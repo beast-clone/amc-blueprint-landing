11dcefcfe6db1df042110154f867d12a5c10963b
beast-clone <beast-clone@users.noreply.github.com>
2026-06-19 16:52:35 +0530

    Switch Astro base path to /amc-pathway for new subdirectory URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://beast-clone.github.io',
-  base: '/amc-blueprint-landing',
+  base: '/amc-pathway',
   compressHTML: true,
 });
